b2ba5ddd829c320767fb49a95e90a235348eb809
meer-khan <shahmirkhan519@gmail.com>
2023-12-01 21:57:59 +0500

    added test file

---
+++ ChatGPT/test.py
@@ -0,0 +1,6 @@
+ques = '''Generate an email campaign calendar for {} 
+    with a focus on {} 
+    around {}, 
+    considering {} and {}, targeted at {}.'''.format("barcelnoa", "ok", "ok", "ok", "ok", "ok")
+
+print(ques)

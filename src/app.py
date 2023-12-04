a7f85d410f3c3656df226d11a1c02ebaa426a5e6
meer-khan <shahmirkhan519@gmail.com>
2023-12-05 00:48:22 +0500

    updated prompt for prompt engineering to create tabular response

--- src/app.py
+++ src/app.py
@@ -23,39 +23,39 @@ cors = CORS(app, resources={r"/prompt/": {"origins": config("ORIGIN")}})
 # Get a list of values
 # allowed_hosts = config('ALLOWED_HOSTS', default='', cast=lambda v: [s.strip() for s in v.split(',')])
 
-key = config("KEY")
-openai.api_key = key
+# key = config("KEY")
+# openai.api_key = key
 
 
-response = openai.ChatCompletion.create(
-  model="gpt-4-1106-preview",
-  messages=[
-        {"role": "user", "content": ""},
-    ]
-)
+# response = openai.ChatCompletion.create(
+#   model="gpt-4-1106-preview",
+#   messages=[
+#         {"role": "user", "content": ""},
+#     ]
+# )
 
-def update_chat(messages, role, content):
-    messages.append({"role": role, "content": content})
-    return messages
-
-
-def get_chatgpt_response(messages):
-  response = openai.ChatCompletion.create(
-  model="gpt-4-1106-preview",
-  messages=messages
-)
-  return  response['choices'][0]['message']['content']
+# def update_chat(messages, role, content):
+#     messages.append({"role": role, "content": content})
+#     return messages
 
 
-def print_last_message(messages):
-    if messages:
-        last_message = messages[-1]
-        print(last_message["role"] + ": " + last_message["content"])
+# def get_chatgpt_response(messages):
+#   response = openai.ChatCompletion.create(
+#   model="gpt-4-1106-preview",
+#   messages=messages
+# )
+#   return  response['choices'][0]['message']['content']
 
-def get_last_message(messages):
-    if messages:
-        last_message = messages[-1]
-        return last_message["role"] + ": " + last_message["content"]
+
+# def print_last_message(messages):
+#     if messages:
+#         last_message = messages[-1]
+#         print(last_message["role"] + ": " + last_message["content"])
+
+# def get_last_message(messages):
+#     if messages:
+#         last_message = messages[-1]
+#         return last_message["role"] + ": " + last_message["content"]
 
 '''
 You are an email marketer for online [sneaker e-commerce store] , 
@@ -78,6 +78,26 @@ Islamabad Pakistan]
 
 # '''
 
+
+# PREVIOUS PARAMS
+'''
+
+{
+    "shop_categories": "sports", 
+    "ccn": "stay healthy and physically healthy",  //Content concept and narrative
+    "seasionality": "winter", 
+    //"ked": "winter holidays", //Key E-commerce Dates
+    //"iskd": "", //Industry-Specific Key Days
+    "shop_locations": "4802 Rockwell Lane,  Rocky Mount, North Carolina"
+}
+
+'''
+
+
+
+
+
+
 @app.route('/prompt/',methods=['POST'])
 def pom_extractor():
     try:
@@ -86,12 +106,12 @@ def pom_extractor():
         {"role": "assistant", "content": "   "},
         ]
 
-
         '''Generate an email campaign calendar for [Shop Categories] 
         with a focus on [Content concept and narrative] 
         around [seasonality], 
         considering [Key E-commerce Dates] and [Industry-Specific Key Days], targeted at [Shop Locations].'''
         data = request.get_json()
+        # ques = data.get("question","")
 
         shop_categories = data.get("shop_categories", " ")
         content_concept_n_narrative = data.get("ccn", " ")
@@ -101,10 +121,47 @@ def pom_extractor():
         shop_locations = data.get("shop_locations"," ")
 
         # * OUR STRING FOR PROMPT ENGINEERING
-        ques = f'''Generate an email campaign calendar for {shop_categories} 
-        with a focus on {content_concept_n_narrative} 
-        around {seasionality}, 
-        considering {key_e_commerce_dates} and {industry_specific_key_days}, targeted at {shop_locations}.'''
+        # ques = f'''Generate an email campaign calendar for {shop_categories} 
+        # with a focus on {content_concept_n_narrative} 
+        # around {seasionality}, 
+        # considering {key_e_commerce_dates} and {industry_specific_key_days}, targeted at {shop_locations}.'''
+
+
+        # *: NEW CODE
+        ques = f'''
+                Please create and rephrase Prompt for weekly calendar campaigns, next 3 months from now, for the shopify store
+                {shop_categories} with a focus on {content_concept_n_narrative}
+                around {seasionality}, considering {key_e_commerce_dates} and {industry_specific_key_days},
+                targeted at {shop_locations}, 
+                consider yourself as strategist and implementor and also emphasize to generate data in tabular. 
+                I want you to return a table with campaign ideas, reasoning, targeted audiences, Prompt should be 
+                tailored according to seasonality, trends, shop category, what the shops products are, key e-commerce days, 
+                3 days a week at least. 
+            '''
+# Please act like a strategist and implementor and generate relevant chatgpt prompt to get above results.
+# Please generate relevant chatgpt prompt to get above results.
+        # ques = '''
+        #     Please create and rephrase only Prompt for weekly calendar campaigns, next 3 months from now, for the 
+        #     shopify “sports and lifestyle clothing  with a focus Stay physically healtyh and active, around winter at  
+        #     4802 Rockwell Lane,  Rocky Mount, North Carolina”, consider yourself as strategist and implementor and also 
+        #     emphasize to generate data in tabular. I want you to return a table with campaign ideas, reasoning, 
+        #     targeted audiences, Prompt should be tailored according to seasonality, trends, shop category, what the shops products
+        #     are, key e-commerce days, 3 days a week at least.
+
+        #     '''
+
+        # ques = '''
+        #     Please create and rephrase only Prompt for weekly calendar campaigns, 
+        #     next 3 months from now, for the shopify sports and lifestyle clothing  with a focus Stay physically 
+        #     healtyh and active, around winter at  4802 Rockwell Lane,  Rocky Mount, North Carolina, 
+
+        #     consider yourself as strategist and implementor, Prompt should be tailored according to seasonality, 
+        #     trends, shop category, what the shops products are, key e-commerce days,
+        #     '''
+
+        # *: END CODE
+
+
 
 
         messages = update_chat(messages, "user", ques)
@@ -125,7 +182,39 @@ def pom_extractor():
 
 
 if __name__ == "__main__":
-    
+
+    key = config("KEY")
+    openai.api_key = key
+    response = openai.ChatCompletion.create(
+    model="gpt-4-1106-preview",
+    messages=[
+            {"role": "user", "content": ""},
+        ]
+    )
+
+    def update_chat(messages, role, content):
+        messages.append({"role": role, "content": content})
+        return messages
+
+
+    def get_chatgpt_response(messages):
+        response = openai.ChatCompletion.create(
+        model="gpt-4-1106-preview",
+        messages=messages
+        )
+        return  response['choices'][0]['message']['content']
+
+
+    def print_last_message(messages):
+        if messages:
+            last_message = messages[-1]
+            print(last_message["role"] + ": " + last_message["content"])
+
+    def get_last_message(messages):
+        if messages:
+            last_message = messages[-1]
+            return last_message["role"] + ": " + last_message["content"]
+        
     if config("MODE") == 'DEV':
         app.run(host='localhost', debug=True, port=5000)
     if config("MODE") == 'PROD':

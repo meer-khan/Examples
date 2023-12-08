81a6c55539805fffe31d794b8a30eb8a322ba601
meer-khan <shahmirkhan519@gmail.com>
2023-12-08 06:43:53 +0500

    db conncection

---
+++ iot/db.py
@@ -0,0 +1,61 @@
+from pymongo import MongoClient
+
+from datetime import datetime
+
+
+def create_db(client):
+    try:
+        database_name = "CoffeeShop"
+
+    # Create or access the specified database
+        existing_db = client[database_name]
+
+    # Insert a document into a collection (this will create the database if it doesn't exist)
+        collection_users = existing_db["Users"]
+        collection_sites = existing_db["Sites"]
+        collection_data = existing_db["Data"]
+        return collection_users,collection_sites,collection_data
+    except Exception as e:
+        print(f"Error: {e}")
+
+# Create collections in the existing database
+
+
+# Create or access the specified database
+# db = client[database_name]
+# print(client.list_database_names())
+# UserID
+# Brand Name
+# Customer email address(User name)
+# Password
+# Location
+
+
+
+# DataID
+# UserID
+# Site ID
+# Timestamp
+# Number of people inside
+# Total trafic
+# Total male
+# Total female
+# Total kids
+
+# def add(cu,cs,cd):
+#     cu.insert_one({"UserID": 1, "BrandName": "Starbucks", "CustomerEmail": "example@gmail.com", "Password": "1234", "Location": "F-10, Islamabad" })
+#     cs.insert_one({"UserID": 1, "SiteID": "1_10", "Location": "F-9, Park, Islamabad" })
+#     cd.insert_one({"DataID": 1, "SiteID": "1_10", "UserID":1, "TimeStamp": datetime.now() , "NoOfPeople": "10", "Totaltrafic":"12",
+#                     "TotalMale": 2, "TotalFemale": 6, "TotalKids": 2 })
+
+
+def main():
+    client = MongoClient("mongodb://localhost:27017/")
+    cu,cs,cd = create_db(client)
+    return cu,cs,cd
+
+
+if __name__ == "__main__": 
+    client = MongoClient("mongodb://localhost:27017/")
+    cu,cs,cd = create_db(client)
+    # add(cu,cs,cd)

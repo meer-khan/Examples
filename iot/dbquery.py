34e051177773785057601c80764731ba81edde87
meer-khan <shahmirkhan519@gmail.com>
2023-12-09 15:19:55 +0500

    refined queries

--- iot/dbquery.py
+++ iot/dbquery.py
@@ -1,22 +1,26 @@
 from pymongo import MongoClient
 from icecream import ic
 from datetime import datetime
+from bson import ObjectId
+import hashlib 
 
 
-def add_user(cu,cs,cd):
-    cu.insert_one({"UserID": 1, "BrandName": "Starbucks", "CustomerEmail": "example@gmail.com", "Password": "1234", "Location": "F-10, Islamabad" })
-    
+def add_user(cu,brand_name, customer_email, password, location):
+    inserted_record = cu.insert_one({"UserID": 1, "BrandName": brand_name, "CustomerEmail": customer_email, "Password": hashlib.sha256(str(password).encode()).hexdigest(), "Location": location })
+    return str(inserted_record.inserted_id)
 
 def add_site(cs, user_id,  location):
     inserted_record = cs.insert_one({"UserID": user_id, "Location": location })
     return str(inserted_record.inserted_id)
 
-def add_data(cd,data):
-    cd.insert_one({"DataID": 1, "SiteID": "1_10", "UserID":1, "TimeStamp": datetime.now() , "NoOfPeople": "10", "Totaltrafic":"12",
-                    "TotalMale": 2, "TotalFemale": 6, "TotalKids": 2 })
+def add_data(cd,site_id,user_id,no_of_people, total_traffic, total_male, total_female, total_kids):
+    cd.insert_one({"DataID": 1, "SiteID": site_id, "UserID":user_id, "TimeStamp": datetime.now() , "NoOfPeople": no_of_people, "Totaltrafic":total_traffic,
+                    "TotalMale": total_male, "TotalFemale": total_female, "TotalKids": total_kids })
 
-def get_one_user(cu, data):
-    found_user = cu.find_one({"_id": data.get("id")})
+def get_one_user(cu, user_id):
+    ic(user_id)
+    found_user = cu.find_one({"_id": ObjectId(user_id)})
+
     if found_user:
         return found_user
     else:
@@ -25,7 +29,7 @@ def get_one_user(cu, data):
 
 def get_users(cu):
     users = []
-    all_users = cu.find()
+    all_users = cu.find({}, {"Password": 0, "UserID":0})
     for user in all_users:
         # ic(type(user))
         # ic(str(user.get("_id")))

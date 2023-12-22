18525ef3c5b9f2ec7d9414955a22bb7c71621120
meer-khan <shahmirkhan519@gmail.com>
2023-12-23 01:39:41 +0500

    updated time with saudi time

--- iot/dbquery.py
+++ iot/dbquery.py
@@ -14,7 +14,12 @@ def add_site(cs, user_id,  location, total_capacity):
     return str(inserted_record.inserted_id)
 
 def add_data(cd,site_id,user_id,no_of_people, total_traffic, total_male, total_female, total_kids):
-    cd.insert_one({"DataID": 1, "SiteID": site_id, "UserID":user_id, "TimeStamp": datetime.utcnow() , "NoOfPeople": no_of_people, "Totaltrafic":total_traffic,
+    sa_tz = pytz.timezone('Asia/Riyadh')
+    local_time = datetime.now(sa_tz)
+
+    # Convert local time to UTC
+    # utc_time = local_time.astimezone(pytz.utc)
+    cd.insert_one({"DataID": 1, "SiteID": site_id, "UserID":user_id, "TimeStamp": local_time , "NoOfPeople": no_of_people, "Totaltrafic":total_traffic,
                     "TotalMale": total_male, "TotalFemale": total_female, "TotalKids": total_kids })
 
 def get_one_user(cu, user_id):

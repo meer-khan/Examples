22500abe5f77c67563b94ff30be7575c7d4b47dd
meer-khan <shahmirkhan519@gmail.com>
2024-01-11 19:10:43 +0500

    attached cors headers

--- iot/app.py
+++ iot/app.py
@@ -24,7 +24,8 @@ cors = CORS(app, resources={r"/users/": {"origins": allowed_origins},
                               r"/analytics/total_visits/": {"origins": allowed_origins},
                                r"/analytics/average_visits/": {"origins": allowed_origins},
                                 r"/analytics/gender_trends/": {"origins": allowed_origins},
-                                 r"/analytics/avg_gender_trends/": {"origins": allowed_origins}
+                                 r"/analytics/avg_gender_trends/": {"origins": allowed_origins},
+                                 r"/analytics/busiest_hour/":{"origins": allowed_origins}
                                  })
 
 

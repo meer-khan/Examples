0b69346c4538370a8af6bb7d934a480292026165
meer-khan <shahmirkhan519@gmail.com>
2024-01-21 02:22:50 +0500

    creating models for database

---
+++ flask_authentication/models.py
@@ -0,0 +1,9 @@
+from extensions import db
+from uuid import uuid4
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.String(), primary_key = True, default=str(uuid4()))
+    username = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(), nullable=False)
+    password = db.Column(db.Text())

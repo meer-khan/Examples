9e9a219db4c48d01e66f64b4c55b474fe38d9b90
meer-khan <shahmirkhan519@gmail.com>
2024-01-21 03:27:54 +0500

    added configurations

--- flask_authentication/app.py
+++ flask_authentication/app.py
@@ -1,7 +1,13 @@
 from flask import Flask
-
+from extensions import db
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_prefixed_env()
+    # app.config.from_prefixed_env()
+    app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+    app.config["SQLALCHEMY_ECHO"] = True
+    # initialize exts
+    db.init_app(app)
+
     return app

d5d25aa168cdb9b254bf0efdc8fbc2d54cff5427
meer-khan <shahmirkhan519@gmail.com>
2024-01-28 03:57:34 +0500

    formatted db file

--- iot/db.py
+++ iot/db.py
@@ -9,20 +9,30 @@ def create_db(client):
     try:
         database_name = "CoffeeShop"
 
-    # Create or access the specified database
+        # Create or access the specified database
         existing_db = client[database_name]
 
-    # Insert a document into a collection (this will create the database if it doesn't exist)
+        # Insert a document into a collection (this will create the database if it doesn't exist)
         collection_users = existing_db["Users"]
         collection_sites = existing_db["Sites"]
         collection_data = existing_db["Data"]
         collection_queue_serving_time = existing_db["queueServingTime"]
         collection_counter_idol_time = existing_db["counterIdolTime"]
         collection_customer_order_time = existing_db["customerOrderTime"]
+        # test = existing_db['test']
 
-        return collection_users,collection_sites,collection_data, collection_queue_serving_time, collection_counter_idol_time, collection_customer_order_time
+        return (
+            collection_users,
+            collection_sites,
+            collection_data,
+            collection_queue_serving_time,
+            collection_counter_idol_time,
+            collection_customer_order_time,
+            # test,
+        )
     except Exception as e:
         print(f"Error: {e}")
+
 
 def main():
     client = MongoClient(config("CONNMONGO"))
@@ -30,9 +40,8 @@ def main():
     return cu, cs, cd, cqst, ccit, ccot
 
 
-
-def add_site(cs, user_id,  location):
-    inserted_record = cs.insert_one({"UserID": user_id, "Location": location })
+def add_site(cs, user_id, location):
+    inserted_record = cs.insert_one({"UserID": user_id, "Location": location})
     return str(inserted_record.inserted_id)
 
 
@@ -47,10 +56,8 @@ def get_one_user(cu, user_id):
         return None
 
 
-
-
 # def db_connnection():
-# # if __name__ == "__main__": 
+# # if __name__ == "__main__":
 #     client = MongoClient("mongodb://localhost:27017/")
 #     cu,cs,cd = create_db(client)
 #     result = add_site(cs, "25", "Islamabad")

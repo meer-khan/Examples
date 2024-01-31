52df75efc05589c38797af561f76217e8adc8e78
meer-khan <shahmirkhan519@gmail.com>
2024-01-31 06:45:40 +0500

    added routes

--- iot2/main.py
+++ iot2/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
-from routers import data_routes
+from routers import data_routes, user_routes
+import db 
 
 app = FastAPI()
+cu, cs, cd, cqst, ccit, ccot = db.main()
 
 app.include_router(data_routes.router)
+app.include_router(user_routes.router)

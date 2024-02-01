bb93079e945ec22c3c77b896b6204e416b9a3eed
meer-khan <shahmirkhan519@gmail.com>
2024-02-02 03:14:17 +0500

    refined schemas

--- iot2/schemas.py
+++ iot2/schemas.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field, field_validator, PydanticUserError, ConfigDict
+from bson import ObjectId
 
 class TrafficInfo(BaseModel):
     siteID: str
@@ -30,15 +31,58 @@ class AdminLogin(BaseModel):
 class SiteRegistration(BaseModel):
     adminId: str
     email: EmailStr
+    name: str
     location: str
     totalCapacity:int
     longitude: float
     latitude: float
 
-class SiteLogin(BaseModel):
+class Login(BaseModel):
     email: str
     password: str
 
+class GetProfile(BaseModel):
+    token:str
+
+class ObjectIdStr(str):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v) -> str:
+        if not isinstance(v, ObjectId):
+            raise ValueError('Not a valid ObjectId')
+        return str(v)
+
+class SiteProfile(BaseModel): 
+    def __init__(self, **data):
+        
+        super().__init__(**data)
+        print(f"SiteProfile instance created with values: {self.model_dump()}")
+        
+    _id: ObjectId
+    email: EmailStr
+    location: str
+    name:str
+    totalCapacity:int
+    cordinates: dict
+
+# try:
+class AdminProfile(BaseModel): 
+    id : str
+    name:str
+    location:str
+
+# except PydanticUserError as exc_info:
+#     print("*****************")
+#     print(exc_info)
+#     print("**********************")
+#     assert exc_info.code == 'decorator-missing-field'
+
+class TokenData(BaseModel):
+    email: EmailStr
+    id: str
 
 class Admin(BaseModel):
     pass 

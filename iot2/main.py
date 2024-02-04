e4ada64c11d20b1a0ba9a2cb997c35c0e9eaf7ad
meer-khan <shahmirkhan519@gmail.com>
2024-02-04 19:43:39 +0500

    updated with getting new collection

--- iot2/main.py
+++ iot2/main.py
@@ -3,7 +3,7 @@ from routers import data_routes, user_routes
 import db 
 
 app = FastAPI()
-ca, cs, cd, cqst, ccit, ccot = db.main()
+csa, ca, cs, cd, cqst, ccit, ccot = db.main()
 
 app.include_router(data_routes.router)
 app.include_router(user_routes.router)

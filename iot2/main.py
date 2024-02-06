b1a63b208cb848376cd80a1648a7947775bb1f57
meer-khan <shahmirkhan519@gmail.com>
2024-02-07 02:35:28 +0500

    added router

--- iot2/main.py
+++ iot2/main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
 from fastapi.security.oauth2 import OAuth2PasswordBearer
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 from routers import data_routes, site_routes, agg_routes, admin_routes, super_admin_routes
 import db 
 
 app = FastAPI()
 csa, ca, cs, cd, cqst, ccit, ccot = db.main()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app.include_router(data_routes.router)
 app.include_router(site_routes.router)
 app.include_router(agg_routes.router)
 app.include_router(admin_routes.router)
+app.include_router(super_admin_routes.router)
+
+

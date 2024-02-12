e3e419254e94671fcc9cd4349de26d36c9c994b1
meer-khan <shahmirkhan519@gmail.com>
2024-02-13 03:08:43 +0500

    added toggle api

--- iot2/routers/super_admin_routes.py
+++ iot2/routers/super_admin_routes.py
@@ -15,14 +15,14 @@ router = APIRouter(tags=["Admin Routes"], prefix="/super-admin")
 
 
 
-@router.post("/register")
-async def super_admin_registration(): 
-    email= "shahmirkhan519@gmail.com"
-    password = "Pakistan2212"
-    hashed_password = utils.hash(password)
-    result = dbquery.add_super_admin(main.csa, email, hashed_password)
-    ic(result)
-    return {"msg": "Super Admin Registration Successful"}
+# @router.post("/register")
+# async def super_admin_registration(): 
+#     email= "shahmirkhan519@gmail.com"
+#     password = "Pakistan2212"
+#     hashed_password = utils.hash(password)
+#     result = dbquery.add_super_admin(main.csa, email, hashed_password)
+#     ic(result)
+#     return {"msg": "Super Admin Registration Successful"}
 
 
 
@@ -92,3 +92,35 @@ async def admin_profile(response: Response, token:str = Depends(main.oauth2_sche
     return  HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Data not Found"
         )
+
+
+@router.patch("/profile", status_code= status.HTTP_204_NO_CONTENT)
+async def super_admin_profile_update(response: Response):
+    ...
+
+
+@router.put("/profile/{site_id}/{field_name}")
+async def admin_profile_update(site_id: str, field_name: str, response: Response, token:str = Depends(main.oauth2_scheme)):
+    try:
+        # Validate field_name
+        valid_fields = ["active_status", "show_data"]
+        if field_name not in valid_fields:
+            raise HTTPException(status_code=400, detail=f"Invalid field_name. Supported fields: {valid_fields}")
+        
+        result = oauth.get_current_user(token=token)
+
+        if isinstance(result, HTTPException):
+            response.status_code = status.HTTP_401_UNAUTHORIZED
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid credentials: Token Expired- Try Login again"
+            )
+        
+        super_admin = dbquery.get_admin(main.ca, result.email, result.id)
+        
+        if super_admin: 
+            dbquery.update_site(main.cs, field_name=field_name, site_id=site_id)
+
+        return {"message": f"{field_name} toggled for item {site_id}"}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))

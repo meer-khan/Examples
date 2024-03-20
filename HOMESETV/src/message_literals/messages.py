7853ad7731130db1df79f4ae4212e831f0d51f15
meer-khan <shahmirkhan519@gmail.com>
2024-03-21 03:30:39 +0500

    added mesg literals

---
+++ HOMESETV/src/message_literals/messages.py
@@ -0,0 +1,12 @@
+class ExceptionLiterals:
+    INVALID_TOKEN = "invalid token - try login again "
+    INVALID_CREDENTIALS = "invalid credentials "
+    ID_NOT_FOUND = "user does not exists "
+    EMAIL_ALREADY_EXISTS = "email already exists "
+    DATA_NOT_FOUND = "data not found "
+    ACCESS_REVOKED = "node access is revoked due to pending payment"
+
+class SuccessLiterals:
+    REGISTRATION_SUCCESSFUL = "registration successful "
+    LOGIN_SUCCESSFUL = "login successful "
+    INSERT_SUCCESSFUL = "data added successfully"

02a01034139c0a94ce53abec9d03a8712fc3aac3
meer-khan <shahmirkhan519@gmail.com>
2024-03-23 03:35:24 +0500

    added find query in db_query

--- HOMESETV/src/db/db_query.py
+++ HOMESETV/src/db/db_query.py
@@ -1,12 +1,15 @@
 import pymongo 
 from bson import ObjectId
 from icecream import ic
-def insert_records(collection, data):
-    ic("***********************")
-    ic(data)
+
+def insert_records(collection, data:dict):
     inserted_record = collection.insert_one(data)
     return inserted_record
 
-def update_records(collection, id, **kwargs):
+def find_user(collection, data_field:str): 
+    record = collection.find_one({"email": data_field})
+    return record
+
+def update_records(collection, id:str, **kwargs):
     updated_record = collection.update_one({"_id": ObjectId(id), "$set": kwargs})
     return updated_record

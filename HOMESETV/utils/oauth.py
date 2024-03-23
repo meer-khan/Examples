c17328151213fe035c2932b8c7ac5c3e951872a7
meer-khan <shahmirkhan519@gmail.com>
2024-03-24 03:45:41 +0500

    added functions for refresh token

--- HOMESETV/utils/oauth.py
+++ HOMESETV/utils/oauth.py
@@ -12,7 +12,7 @@ oauth_scheme = OAuth2PasswordBearer(tokenUrl="login")
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 1440
-
+REFRESH_TOKEN_EXPIRE_DAYS = 7 
 
 def create_access_token(data:dict):
     to_encode = data.copy()
@@ -22,11 +22,11 @@ def create_access_token(data:dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+
 def verify_token(token:str, credentials_exception):
 
     try:
         payload = jwt.decode(token,SECRET_KEY, algorithms=ALGORITHM)
-        ic(payload)
         email: str = payload.get("email")
         id :str = payload.get("id")
 
@@ -38,7 +38,38 @@ def verify_token(token:str, credentials_exception):
         return credentials_exception
     
     return token_data
+
+
+
+def create_refresh_token(data:dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    to_encode.update({"exp": expire})
+    ic(to_encode)
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def verify_refresh_token(token:str):
+    try:
+        payload = jwt.decode(token,SECRET_KEY, algorithms=ALGORITHM)
+        id :str = payload.get("id")
+
+        if id is None: 
+            return HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, 
+                detail = "Unauthorized user",
+                headers={"WWW.Authenticate": "Bearer"})
+
+    except JWTError: 
+        return HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED, 
+        detail = "Unauthorized user",
+        headers={"WWW.Authenticate": "Bearer"})
     
+    return id
+
+
 
 def get_current_user(token: str = Depends(oauth_scheme)):
     credentials_exception =  HTTPException(

3aa64894ec6afb21519cf7f477d02ce563178b92
meer-khan <shahmirkhan519@gmail.com>
2024-03-24 03:45:23 +0500

    added queries

--- HOMESETV/src/db/db_query.py
+++ HOMESETV/src/db/db_query.py
@@ -1,6 +1,7 @@
 import pymongo 
 from bson import ObjectId
 from icecream import ic
+from typing_extensions import List
 
 def insert_records(collection, data:dict):
     inserted_record = collection.insert_one(data)
@@ -9,6 +10,14 @@ def insert_records(collection, data:dict):
 def find_user(collection, data_field:str): 
     record = collection.find_one({"email": data_field})
     return record
+
+def find_single_record(collection,  data:dict): 
+    record = collection.find_one(data)
+    return record
+
+def find_multiple_records(collection, data_dict)-> List: 
+    records_list = collection.find(data_dict)
+    return records_list
 
 def update_records(collection, id:str, **kwargs):
     updated_record = collection.update_one({"_id": ObjectId(id), "$set": kwargs})

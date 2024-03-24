ce8dfcca482ab9bfb97f90305f7b54bd66ad1ddf
meer-khan <shahmirkhan519@gmail.com>
2024-03-24 23:36:21 +0500

    updated schemas for profile ret

--- HOMESETV/src/schemas/schemas.py
+++ HOMESETV/src/schemas/schemas.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel, EmailStr, StringConstraints, field_validator, model_validator
+from pydantic import BaseModel, EmailStr, StringConstraints, field_validator, model_validator, Field, field_serializer
 from pydantic_extra_types import phone_numbers
-from typing_extensions import Annotated, Dict, Any
+from typing_extensions import Annotated, Dict, Any, List
 
 class Login(BaseModel):
     email: EmailStr
@@ -32,3 +32,24 @@ class Signup(BaseModel):
         if pw1 is not None and pw2 is not None and pw1 != pw2:
             raise ValueError('passwords do not match')
         return self
+
+class ProfileRET(BaseModel): 
+    id: str = Field(alias="_id")
+    email: EmailStr
+    firstName: str
+    lastName: str
+    phoneNo: str
+    roles: List[str]
+    plan: None | str
+
+    
+
+    # '_id': ObjectId('65fdf3e0ebbe8495813aa115'),
+    #          'email': 'shahmirkhan520@gmail.com',
+    #          'firstName': 'Shahmeer Khan',
+    #          'lastName': 'Khan',
+    #          'password': '$2b$12$Uf8tXw2PhCV2rcpglq4Wru7PDPQB616eUrWVSRjHAA0pZgYQiqPeK',
+    #          'phoneNo': '0985',
+    #          'plan': None,
+    #          'roles': ['user'],
+    #          'termsConditions': True

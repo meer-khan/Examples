26692c8716c71d9f07d5612a3ad35aed956069e5
meer-khan <shahmirkhan519@gmail.com>
2024-03-24 23:37:39 +0500

    working on profile route

--- HOMESETV/src/routers/profile_route.py
+++ HOMESETV/src/routers/profile_route.py
@@ -1,8 +1,18 @@
-from fastapi import status, APIRouter, Response
+from fastapi import status, APIRouter, Response, Depends
 from icecream import ic
-
+from utils import oauth
+from db import db_query
+import app
+from bson import ObjectId
+from schemas import schemas
 router = APIRouter(tags=["profile"])
 
-@router.post("/profile", status_code=status.HTTP_200_OK)
-async def profile():
-   return {"msg": "profile route is working fine"}
+@router.post("/profile", status_code=status.HTTP_200_OK, response_model=schemas.ProfileRET)
+async def profile(response: Response, user_data = Depends(oauth.get_current_user)):
+   ic(user_data)
+   email = user_data[0]
+   id = user_data[1]
+   quer_dict = {"_id": ObjectId(id), "email": email}
+   result = db_query.find_single_record(app.col_user, data_dict=quer_dict)
+   ic(result)
+   return result

ee04a498635c7f015a50dda3398305a2cc522286
meer-khan <shahmirkhan519@gmail.com>
2024-03-25 02:50:30 +0500

    updated login route

--- HOMESETV/src/routers/login_route.py
+++ HOMESETV/src/routers/login_route.py
@@ -1,11 +1,12 @@
 from fastapi import status, APIRouter, Response, HTTPException, Depends
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from typing_extensions import Dict, Any
 from db import db_query
 from utils import oauth, password_manager
 from icecream import ic
 import app
 
-router = APIRouter(tags=["login"], prefix="/user")
+router = APIRouter(tags=["user-management"], prefix="/user")
 
 @router.post("/login", status_code=status.HTTP_200_OK)
 async def login( response: Response, user_credentials: OAuth2PasswordRequestForm = Depends()):
@@ -14,13 +15,13 @@ async def login( response: Response, user_credentials: OAuth2PasswordRequestForm
       user:dict = db_query.find_user(app.col_user, user_credentials.username)
       ic(user)
       if not user:
-         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user not found")
+         return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user not found")
       
       else:
          result = password_manager.verify(user_credentials.password, user.get("password"))
          if not result: 
             response.status_code = status.HTTP_401_UNAUTHORIZED
-            raise HTTPException(
+            return HTTPException(
                     status_code=status.HTTP_401_UNAUTHORIZED, detail= "user not found"
                 )
          else:
@@ -28,50 +29,20 @@ async def login( response: Response, user_credentials: OAuth2PasswordRequestForm
                 {"email": user.get("email"), "id": str(user.get("_id"))}
             )
 
-            return {
-                "access_token": access_token,
-                "token_type": "bearer",
-                "role": user.get("role")
-            }
+            refresh_token = oauth.create_refresh_token({"id": str(user.get("_id"))})
 
+            return {
+                "accessToken": access_token,
+                "refreshToken": refresh_token,
+                "tokenType": "bearer",
+                "roles": user.get("roles")
+            }
+         
    except Exception as ex:   
       # TODO: LOG - C {ex}
       response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+      ic(ex)
       return HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail= "internal server error"
         )
 
-
-# @router.post("/refresh-token", status_code=status.HTTP_200_OK)
-# async def refresh_token(refreshToken:str, response: Response):
-#    try: 
-#       id = oauth.verify_refresh_token(refreshToken)
-#       user:dict = db_query.find_user(app.col_user, data_field=, data)
-#       ic(user)
-#       if not user:
-#          raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user not found")
-      
-#       else:
-#          result = password_manager.verify(user_credentials.password, user.get("password"))
-#          if not result: 
-#             response.status_code = status.HTTP_401_UNAUTHORIZED
-#             raise HTTPException(
-#                     status_code=status.HTTP_401_UNAUTHORIZED, detail= "user not found"
-#                 )
-#          else:
-#             access_token = oauth.create_access_token(
-#                 {"email": user.get("email"), "id": str(user.get("_id"))}
-#             )
-
-#             return {
-#                 "access_token": access_token,
-#                 "token_type": "bearer",
-#                 "role": user.get("role")
-#             }
-
-#    except Exception as ex:   
-#       # TODO: LOG - C {ex}
-#       response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-#       return HTTPException(
-#             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail= "internal server error"
-#         )

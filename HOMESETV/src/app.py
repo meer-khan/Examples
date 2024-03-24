78843872b2750dad09defe1f48d0400dce739389
meer-khan <shahmirkhan519@gmail.com>
2024-03-24 23:36:32 +0500

    enabled profile routes in app

--- HOMESETV/src/app.py
+++ HOMESETV/src/app.py
@@ -9,7 +9,7 @@ from fastapi.security.oauth2 import OAuth2PasswordBearer
 from icecream import ic
 # from routers import login_route, signup_route, profile_route
 from db.db_connection import db_creation
-from routers import signup_route, login_route
+from routers import signup_route, login_route, profile_route
 
 
 
@@ -20,4 +20,4 @@ col_user, col_roles, col_plan = db_creation()
 
 app.include_router(login_route.router)
 app.include_router(signup_route.router)
-# app.include_router(profile_route.router)
+app.include_router(profile_route.router)

d04d5a5fcc75330bcdf1b12236df5d37c0acd3ae
meer-khan <shahmirkhan519@gmail.com>
2024-03-25 02:53:12 +0500

    formatted signup code and removed commented code

--- HOMESETV/src/routers/signup_route.py
+++ HOMESETV/src/routers/signup_route.py
@@ -33,18 +33,18 @@ async def signup(
         ic(user_data)
         try:
             user_data = schemas.Signup(
-                
-                    userName = userName,
-                    email=  email,
-                    password1= password1,
-                    password2=  password2,
-                    termsConditions = termsConditions,
-                
+                userName=userName,
+                email=email,
+                password1=password1,
+                password2=password2,
+                termsConditions=termsConditions,
             )
         except ValidationError as exc_info:
             ic(exc_info)
             response.status_code = status.HTTP_400_BAD_REQUEST
-            return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=exc_info.errors())
+            return HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail=exc_info.errors()
+            )
 
         user_data = user_data.model_dump()
         user_data["password"] = password_manager.hash(user_data["password1"])
@@ -73,55 +73,3 @@ async def signup(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="internal server error",
         )
-
-
-# @router.post("/registration", status_code=status.HTTP_201_CREATED)
-# def admin_registration(
-#     data: schemas.AdminRegistration,
-#     response: Response,
-#     token: str = Depends(main.oauth2_scheme),
-# ):
-
-#     result = oauth.get_current_user(token=token)
-
-#     if isinstance(result, HTTPException):
-#         response.status_code = status.HTTP_401_UNAUTHORIZED
-#         raise HTTPException(
-#             status_code=status.HTTP_401_UNAUTHORIZED,
-#             detail= ExceptionLiterals.INVALID_TOKEN,
-#         )
-#     try:
-#         user = dbquery.get_one_user(collection_user=main.csa, user_id=result.id)
-#         if not user:
-#             response.status_code = status.HTTP_401_UNAUTHORIZED
-#             return HTTPException(
-#                 status_code=status.HTTP_401_UNAUTHORIZED,
-#                 detail= ExceptionLiterals.ID_NOT_FOUND,
-#             )
-
-#         # generate random password 5
-#         # EcT/5~F\" 520@gmail
-#         random_password = utils.generate_random_password(8)
-#         hashed_password = utils.hash(random_password)
-#         try:
-#             admin_id = dbquery.add_admin(
-#                 main.ca, email=data.email, password=hashed_password
-#             )
-#             return {
-#                 "msg": SuccessLiterals.REGISTRATION_SUCCESSFUL,
-#                 "adminId": admin_id,
-#                 "email": data.email,
-#                 "password": random_password,
-#             }
-
-#         except errors.DuplicateKeyError as ex:
-#             response.status_code = status.HTTP_409_CONFLICT
-#             return HTTPException(
-#                 status_code=status.HTTP_409_CONFLICT,
-#                 detail= ExceptionLiterals.EMAIL_ALREADY_EXISTS + data.email,
-#             )
-#     except Exception as ex:
-#         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-#         return HTTPException(
-#             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(ex)
-#         )

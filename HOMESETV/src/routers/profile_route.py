02b673e9624fab377c25dc75788e7eb32bea47f0
meer-khan <shahmirkhan519@gmail.com>
2024-03-28 01:20:39 +0500

    updated profile route with delete functionality

--- HOMESETV/src/routers/profile_route.py
+++ HOMESETV/src/routers/profile_route.py
@@ -24,7 +24,7 @@ async def profile(response: Response, user_data:dict=Depends(oauth.get_current_u
             status_code=status.HTTP_404_NOT_FOUND, detail="user not found"
         )
 
-
+# Change password
 @router.put("/profile", status_code=status.HTTP_200_OK)
 async def update_password(
    response: Response,
@@ -55,8 +55,7 @@ async def update_password(
                filter_query ={"_id": ObjectId(user_data.get("sub"))}
                query_dict = {
                   "$set": {
-                     "password": password_manager.hash(passwords.newPassword),
-                     "updatedAt": datetime.datetime.now(datetime.UTC),
+                     "active"
                   },
                   "$inc": {"passVer": 1} 
                }
@@ -80,6 +79,40 @@ async def update_password(
 
 
 
+# Delete Profile
+@router.delete("/profile", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_account(
+   response: Response,
+   data: schemas.DelAccount,
+   user_data:dict=Depends(oauth.get_current_user),
+   ):
+   query_dict = {"_id": ObjectId(user_data.get("sub")), "active": True, "passVer": user_data.get("ver")}
+   query_result:dict = db_query.find_single_record(app.col_user, data_dict=query_dict)
 
+   #  if user exists
+   if query_result:
+      # check current password is same as stored password or not
+      pass_result = password_manager.verify(
+         data.password, query_result.get("password")
+      )
 
+      # if current password and stored password is same -> delete the user
+      if pass_result:
+         # query to set the active status of account as False
+               filter_query =  {"_id": ObjectId(user_data.get("sub"))}
+               query_dict = {"$set": {"active": False}}
+               db_query.update_single_record(app.col_user, filter_query, query_dict)
+               return {
+                   "detail": "account deleted successfully"
+               }
+      else:
+         # if current password and stored passwords are not same
+         raise HTTPException(
+               status_code=status.HTTP_400_BAD_REQUEST,
+               detail="current password is not valid",
+         )
 
+   else:
+      raise HTTPException(
+         status_code=status.HTTP_404_NOT_FOUND, detail="user not found"
+      )

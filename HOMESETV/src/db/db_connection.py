2c7e626164c6b100c64e1b4a55f34d9372932c4b
meer-khan <shahmirkhan519@gmail.com>
2024-03-28 01:21:02 +0500

    updated db for new collection

--- HOMESETV/src/db/db_connection.py
+++ HOMESETV/src/db/db_connection.py
@@ -14,7 +14,7 @@ def create_db_connection(connection_string: str, db_name: str):
     return db
 
 
-def create_db_collections(collection_users, collection_roles, collections_plans, db):
+def create_db_collections(collection_users, collection_roles, collection_plans, collection_blacklist_token, db):
     """
     Create a new collection in the database.
     """
@@ -24,8 +24,9 @@ def create_db_collections(collection_users, collection_roles, collections_plans,
         # col_roles = db.create_collection(collections_plans)
         col_user = db[collection_users]
         col_roles = db[collection_roles]
-        col_plans = db[collections_plans]
-        return col_user, col_roles, col_plans
+        col_plans = db[collection_plans]
+        col_blacklist_token = db[collection_blacklist_token]
+        return col_user, col_roles, col_plans, col_blacklist_token
 
     except errors.CollectionInvalid as ex:
         raise ex
@@ -63,15 +64,17 @@ def db_creation():
     users_collection = "users"
     roles_collection = "roles"
     plans_collection = "plans"
+    blacklist_token_collection = "blacklistedTokens"
     db_name = config("DB_NAME")
     db_conn_str = config("DB_CONN_STR")
 
     db_instance = create_db_connection(db_conn_str, db_name)
 
-    col_user, col_roles, col_plans = create_db_collections(
+    col_user, col_roles, col_plans, col_blacklist_token = create_db_collections(
         collection_users=users_collection,
         collection_roles=roles_collection,
-        collections_plans=plans_collection,
+        collection_plans=plans_collection,
+        collection_blacklist_token=blacklist_token_collection,
         db=db_instance,
     )
 
@@ -81,4 +84,4 @@ def db_creation():
 
     col_user.create_index("email", unique=True)
     col_roles.create_index("role", unique=True)
-    return col_user, col_roles, col_plans
+    return col_user, col_roles, col_plans, col_blacklist_token
